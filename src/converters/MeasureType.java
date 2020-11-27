b505aeeaf04d227b56da1043fcdda238991544f5
João Victor Corsi Ferreira <jvccorsiferrera@hotmail.com>
2020-11-27 16:57:13 -0300

    Update MeasureType.java

--- src/converters/MeasureType.java
+++ src/converters/MeasureType.java
@@ -4,16 +4,16 @@ public enum MeasureType {
     
     DISTANCE("comprimento", "metre (m)"), //ok
     AREA("area", "square metre (m\\u33a1)"),
-    VOLUME("volume", "litre (L)"),//ok
-    MASS("mass", "kilogram (kg)"),//ok
-    TIME("time", "second (s)"),//ok
-    SPEED("speed", "m/s"), // ok
+    VOLUME("volume", "litre (L)"),
+    MASS("mass", "kilogram (kg)"),
+    TIME("time", "second (s)"),
+    SPEED("speed", "m/s"), 
     ACCELERATION("accelerations", "m/s\u00b2"),
     FORCE("force", "newton (N)"),
     PRESSURE("pressure", "pascal (Pa)"),
     ENERGY("energy", "joule (J)"),
     POWER("power", "watt (W)"),
-    VISCOSITY("viscosity", "Pa·s");// ok
+    VISCOSITY("viscosity", "Pa·s");
    
     private final String type;
     private final String basicUnit;
